1a00a8af18bb5bc46bb8e95a2de89075c8238eb1
Preeti and Nithya <preetithirukonda@gmail.com>
2019-09-30 21:11:17 -0400

    added vuforia into auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModesAuto/LookForSkystone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModesAuto/LookForSkystone.java
@@ -2,24 +2,43 @@ package org.firstinspires.ftc.teamcode.OpModesAuto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Mechanisms.Robot;
+import org.firstinspires.ftc.teamcode.Util.Vuforia;
+import org.firstinspires.ftc.teamcode.Util.Vuforia;
 
 @Autonomous()
 //@Disabled
 public class LookForSkystone extends OpMode {
     private Robot robot = new Robot();
+    private Vuforia vuforia = new Vuforia();
 
     // Code to run ONCE when the driver hits INIT
     @Override
     public void init() {
         robot.init(hardwareMap);
-
+ vuforia.start(hardwareMap);
     }
 
     // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
     @Override
     public void loop() {
+        Vuforia.Location location = vuforia.whereIsSkystone(telemetry);
+        switch(location){
+            case LEFT:
+                robot.strafe(-0.3);
+                break;
+            case RIGHT:
+                robot.strafe(0.3);
+                break;
+            case CENTER:
+                robot.strafe(0);
+                break;
+            case NOT_FOUND:
+                robot.strafe(-0.3);
+                break;
+        }
         robot.strafe(0.3);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestVuforia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestVuforia.java
@@ -290,9 +290,9 @@ public class TestVuforia extends LinearOpMode {
 
         // Next, translate the camera lens to where it is on the robot.
         // In this example, it is centered (left to right), but forward of the middle of the robot, and above ground level.
-        final float CAMERA_FORWARD_DISPLACEMENT  = 4.0f * mmPerInch;   // eg: Camera is 4 Inches in front of robot center
-        final float CAMERA_VERTICAL_DISPLACEMENT = 8.0f * mmPerInch;   // eg: Camera is 8 Inches above ground
-        final float CAMERA_LEFT_DISPLACEMENT     = 0;     // eg: Camera is ON the robot's center line
+        final float CAMERA_FORWARD_DISPLACEMENT  = 2.5f * mmPerInch;   // eg: Camera is 4 Inches in front of robot center
+        final float CAMERA_VERTICAL_DISPLACEMENT = 4.5f * mmPerInch;   // eg: Camera is 8 Inches above ground
+        final float CAMERA_LEFT_DISPLACEMENT     = 2.0f * mmPerInch;     // eg: Camera is ON the robot's center line
 
         OpenGLMatrix robotFromCamera = OpenGLMatrix
                     .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
@@ -344,8 +344,8 @@ public class TestVuforia extends LinearOpMode {
                         translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
 
                 double xPosition = translation.get(0);
-                if (xPosition <-10) {
-                    positionSkystone = "left";
+                    if (xPosition <-10) {
+                        positionSkystone = "left";
                 }else {
                     positionSkystone = "center";
                 }

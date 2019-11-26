de033828e44524cb791480a0cc84cc8ede9a88f0
andrew <atvo173@gmail.com>
2019-11-25 20:48:57 -0500

    added a second snatcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Snatcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Snatcher.java
@@ -8,25 +8,32 @@ import java.util.List;
 
 
 class Snatcher {
-    private double LOWERED_POSITION = 0.6;
-    private double RAISED_POSITION = 1.0;
-    private Servo snatcher;
+    private double RIGHT_LOWERED = 0.6;
+    private double RIGHT_RAISED = 1.0;
+    private Servo snatcherRight;
+    private double LEFT_LOWERED = 1.0;
+    private double LEFT_RAISED = 0.6;
+    private Servo snatcherLeft;
 
     void init(HardwareMap hwmap) {
-        snatcher = hwmap.get(Servo.class, "snatcher");
+        snatcherRight = hwmap.get(Servo.class, "snatcher_right");
+        snatcherLeft = hwmap.get(Servo.class, "snatcher_left");
     }
 
     List<QQ_Test> getTests() {
         return Arrays.asList(
-                new QQ_TestServo("snatcher", 0, 1, snatcher)
+                new QQ_TestServo("snatcher_right", RIGHT_RAISED, RIGHT_LOWERED, snatcherRight) ,
+                new QQ_TestServo("snatcher_left", LEFT_RAISED, LEFT_LOWERED, snatcherLeft)
         );
     }
 
     void lift(){
-        snatcher.setPosition(RAISED_POSITION);
+        snatcherRight.setPosition(RIGHT_RAISED);
+        snatcherLeft.setPosition(LEFT_RAISED);
     }
     void lower() {
-        snatcher.setPosition(LOWERED_POSITION);
+        snatcherRight.setPosition(RIGHT_LOWERED);
+        snatcherLeft.setPosition(LEFT_LOWERED);
     }
 
 }

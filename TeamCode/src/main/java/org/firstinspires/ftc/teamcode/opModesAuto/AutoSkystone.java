701c6d9d8d019e35120f4c0021ca2d263a69b4f9
philip smith <philip@randomsmiths.com>
2020-02-07 20:24:04 -0500

    Comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModesAuto/AutoSkystone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModesAuto/AutoSkystone.java
@@ -39,6 +39,9 @@ public class AutoSkystone extends AutoBase {
     private static double STONE_1_Y_POS = -25;
     private static double STONE_WIDTH = 8;
 
+    /**
+     * Initializes the camera and robot
+     */
     @Override
     public void init() {
         super.init();
@@ -55,6 +58,9 @@ public class AutoSkystone extends AutoBase {
         }
     }
 
+    /**
+     * stops the camera
+     */
     @Override
     public void start() {
         super.start();
@@ -63,7 +69,7 @@ public class AutoSkystone extends AutoBase {
     }
 
     /**
-     * overrides to Force placement in the depot area
+     * overrides to Force placement in the depot area and far park
      *
      * @see AutoBase
      */
@@ -76,6 +82,12 @@ public class AutoSkystone extends AutoBase {
         telemetry.update();
     }
 
+    /**
+     * gets the y position for the stone based on its location
+     *
+     * @param stoneLocation what location is the stone in: 1, 2, or 3
+     * @return returns stone y position
+     */
     private double getStoneYPosition(int stoneLocation) {
         if (redAlliance) {
             switch (stoneLocation) {
@@ -88,7 +100,7 @@ public class AutoSkystone extends AutoBase {
                     return STONE_1_Y_POS;
             }
         } else {
-            switch (stoneLocation) {
+            switch (stoneLocation) { // blue alliance stones go the other way
                 case 1:
                     return STONE_1_Y_POS;
                 case 2:
@@ -100,37 +112,43 @@ public class AutoSkystone extends AutoBase {
         }
     }
 
+    /**
+     * gets the steps for the second stone
+     * @param stoneLocation which position was the stone in
+     * @param redAlliance are we on red alliance
+     * @return returns a QQ_actionList with the steps
+     */
     private QQ_ActionActionList getSecondStoneSteps(int stoneLocation, boolean redAlliance) {
-        int allianceMultiplier = redAlliance ? 1 : -1;
-        int depoDirection = redAlliance ? 180 : 0;
+        int allianceMultiplier = redAlliance ? 1 : -1; // x is flipped for blue alliance
+        int depoDirection = redAlliance ? 180 : 0; // rotation is inverted
         if (redAlliance) {
             switch (stoneLocation) {
                 case 1:
                     return new QQ_ActionActionList("Stone 1", Arrays.asList(
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 8), getStoneYPosition(3) - 24, DistanceUnit.INCH),
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 8), getStoneYPosition(3) - 24, DistanceUnit.INCH), // drive to the right location for a random stone
                             new QQ_ActionActionPair(
                                     new QQ_ActionRotateTo(depoDirection, AngleUnit.DEGREES),
                                     new QQ_ActionLift(3.0, DistanceUnit.INCH)
-                            ),
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X), getStoneYPosition(3) - 24, DistanceUnit.INCH),
+                            ), // rotate and lift lift in parallel to save time
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X), getStoneYPosition(3) - 24, DistanceUnit.INCH), // drive in to the right location to collect the stone
                             new QQ_ActionPincer(true),
-                            new QQ_ActionDelayFor(1),
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 4), getStoneYPosition(3) - 24, DistanceUnit.INCH)
+                            new QQ_ActionDelayFor(1), // grab the stone
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 4), getStoneYPosition(3) - 24, DistanceUnit.INCH) // back up so as to disturb bricks less
 
                     ));
                 case 2:
                 case 3:
-                default:
+                default: //case 2 and 3 and default are all the same
                     return new QQ_ActionActionList("Stone 2 or 3", Arrays.asList(
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 8), getStoneYPosition(stoneLocation) - 26, DistanceUnit.INCH),
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 8), getStoneYPosition(stoneLocation) - 26, DistanceUnit.INCH), // drive to correct location
                             new QQ_ActionActionPair(
                                     new QQ_ActionRotateTo(depoDirection, AngleUnit.DEGREES),
                                     new QQ_ActionLift(3.0, DistanceUnit.INCH)
-                            ),
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X), getStoneYPosition(stoneLocation) - 24, DistanceUnit.INCH),
+                            ), // lift and turn at the same time
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X), getStoneYPosition(stoneLocation) - 24, DistanceUnit.INCH), // go to the correct x to grab stone
                             new QQ_ActionPincer(true),
-                            new QQ_ActionDelayFor(1),
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 4), getStoneYPosition(stoneLocation) - 24, DistanceUnit.INCH)
+                            new QQ_ActionDelayFor(1), // grab stone delay to let servo close
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 4), getStoneYPosition(stoneLocation) - 24, DistanceUnit.INCH) // back up to lessen the disturbance of bricks
 
                     ));
             }
@@ -139,30 +157,30 @@ public class AutoSkystone extends AutoBase {
             switch (stoneLocation) {
                 case 3:
                     return new QQ_ActionActionList("Stone 1", Arrays.asList(
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 8), getStoneYPosition(1) - 24, DistanceUnit.INCH),
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 8), getStoneYPosition(1) - 24, DistanceUnit.INCH), // drive to location of 3rd stone
                             new QQ_ActionActionPair(
                                     new QQ_ActionRotateTo(depoDirection, AngleUnit.DEGREES),
                                     new QQ_ActionLift(3.0, DistanceUnit.INCH)
-                            ),
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X), getStoneYPosition(1) - 24, DistanceUnit.INCH),
+                            ), // lift and turn at the same time
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X), getStoneYPosition(1) - 24, DistanceUnit.INCH), // drive to the correct location to intake a brick
                             new QQ_ActionPincer(true),
-                            new QQ_ActionDelayFor(1),
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 4), getStoneYPosition(1) - 24, DistanceUnit.INCH)
+                            new QQ_ActionDelayFor(1), // grab brick (allowing servo to close)
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 4), getStoneYPosition(1) - 24, DistanceUnit.INCH) // lessen disturbance of the other bricks
 
                     ));
                 case 2:
                 case 1:
-                default:
+                default: // case 2 and 1 and default are the same
                     return new QQ_ActionActionList("Stone 2 or 3", Arrays.asList(
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 2), getStoneYPosition(stoneLocation) - 24, DistanceUnit.INCH),
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 2), getStoneYPosition(stoneLocation) - 24, DistanceUnit.INCH), // drive to correct y for the brick
                             new QQ_ActionActionPair(
                                     new QQ_ActionRotateTo(depoDirection, AngleUnit.DEGREES),
                                     new QQ_ActionLift(3.0, DistanceUnit.INCH)
-                            ),
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X), getStoneYPosition(stoneLocation) - 24, DistanceUnit.INCH),
+                            ), // lift and spin at the same time to decrease time used
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X), getStoneYPosition(stoneLocation) - 24, DistanceUnit.INCH), // go to correct x to intake stone
                             new QQ_ActionPincer(true),
-                            new QQ_ActionDelayFor(1),
-                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 4), getStoneYPosition(stoneLocation) - 24, DistanceUnit.INCH)));
+                            new QQ_ActionDelayFor(1), // grab block (waits for servo to close)
+                            new QQ_ActionDriveTo(allianceMultiplier * (STONE_COLLECTION_RED_X + 4), getStoneYPosition(stoneLocation) - 24, DistanceUnit.INCH))); // lessens the disturbance of the blocks
 
 
             }
@@ -191,78 +209,77 @@ public class AutoSkystone extends AutoBase {
         if (redAlliance) {
             steps.addAll(Arrays.asList(
                     startPosition,
-                    new QQ_ActionRotator(0.0),
+                    new QQ_ActionRotator(0.0), // position rotater to correct location
                     new QQ_ActionActionPair(
                             new QQ_ActionActionPair(
                                     new QQ_ActionLift(3.0, DistanceUnit.INCH),
                                     new QQ_ActionFlipper(90)
                             ),
                             new QQ_ActionDriveTo(STONE_COLLECTION_RED_X, getStoneYPosition(pipeline.stoneLocation), DistanceUnit.INCH)
-                    ),
+                    ), // Lift the lift, flip the flipper and drive to the right spot all at the same time
                     new QQ_ActionLift(0.0, DistanceUnit.CM),
                     new QQ_ActionPincer(true),
-                    new QQ_ActionDelayFor(1),
-                    new QQ_ActionDriveTo(STONE_COLLECTION_RED_X + 4, getStoneYPosition(pipeline.stoneLocation), DistanceUnit.INCH),
-                    new QQ_ActionRotateTo(90, AngleUnit.DEGREES),
+                    new QQ_ActionDelayFor(1), // grab block
+                    new QQ_ActionDriveTo(STONE_COLLECTION_RED_X + 4, getStoneYPosition(pipeline.stoneLocation), DistanceUnit.INCH), // lessen the disturbance of other blocks
+                    new QQ_ActionRotateTo(90, AngleUnit.DEGREES), // don't hit alliance partner
                     new QQ_ActionDriveTo(WAFFLE_RED_X, WAFFLE_RED_Y, DistanceUnit.INCH),
                     new QQ_ActionActionPair(
                             new QQ_ActionActionList("Moving", Arrays.asList(
                                     new QQ_ActionRotateTo(180, AngleUnit.DEGREES),
                                     new QQ_ActionDriveTo(WAFFLE_RED_X - 12, WAFFLE_RED_Y, DistanceUnit.INCH))),
-                            new QQ_ActionLift(11.0, DistanceUnit.INCH)),
+                            new QQ_ActionLift(11.0, DistanceUnit.INCH)), // lift while we turn and drive
                     //new QQ_ActionFlipper(90),
                     new QQ_ActionPincer(false),
                     new QQ_ActionSnatcher(true),
-                    new QQ_ActionDelayFor(1.25),
+                    new QQ_ActionDelayFor(1.25), // release brick and grab foundation
                     new QQ_ActionDriveTo((WAFFLE_RED_X + 18), WAFFLE_RED_Y - 5, DistanceUnit.INCH),
                     new QQ_ActionRotateTo(90, AngleUnit.DEGREES),
-
-
+                    new QQ_ActionDriveTo((WAFFLE_RED_X + 18), WAFFLE_RED_Y + 10, DistanceUnit.INCH), //back up and square on wall
                     new QQ_ActionSnatcher(false),
-                    new QQ_ActionSetPosition(new RobotPosition((WAFFLE_RED_X + 24), FIELD_BOUNDARIES - (WAFFLE_WIDTH + 9), DistanceUnit.INCH, 90, AngleUnit.DEGREES)),
-                    new QQ_ActionDriveTo(37, 24, DistanceUnit.INCH),
-                    secondStone,
-                    new QQ_ActionRotateTo(90.0, AngleUnit.DEGREES)
+                    new QQ_ActionSetPosition(new RobotPosition((WAFFLE_RED_X + 24), FIELD_BOUNDARIES - (WAFFLE_WIDTH + 9), DistanceUnit.INCH, 90, AngleUnit.DEGREES)), // reset where the robot thinks it is
+                    new QQ_ActionDriveTo(37, 24, DistanceUnit.INCH),// clear alliance partner
+                    secondStone, // run second stone steps
+                    new QQ_ActionRotateTo(90.0, AngleUnit.DEGREES) // dont hit alliance partner on the way back
 
 
             ));
         } else {
             steps.addAll(Arrays.asList(
                     startPosition,
-                    new QQ_ActionRotator(0.0),
+                    new QQ_ActionRotator(0.0), // position rotater to correct location
                     new QQ_ActionActionPair(
                             new QQ_ActionActionPair(
                                     new QQ_ActionLift(3.0, DistanceUnit.INCH),
                                     new QQ_ActionFlipper(90)
                             ),
                             new QQ_ActionDriveTo(-STONE_COLLECTION_RED_X, getStoneYPosition(pipeline.stoneLocation), DistanceUnit.INCH)
-                    ),
+                    ), // Lift the lift, flip the flipper and drive to the right spot all at the same time
                     new QQ_ActionLift(0.0, DistanceUnit.CM),
                     new QQ_ActionPincer(true),
                     new QQ_ActionDelayFor(1), // 1st stone grabbed
                     new QQ_ActionDriveTo(-(STONE_COLLECTION_RED_X + 4), getStoneYPosition(pipeline.stoneLocation), DistanceUnit.INCH),
-                    new QQ_ActionRotateTo(90, AngleUnit.DEGREES),
+                    new QQ_ActionRotateTo(90, AngleUnit.DEGREES), // don't hit alliance partner
                     new QQ_ActionDriveTo(-(WAFFLE_RED_X + 8), WAFFLE_RED_Y, DistanceUnit.INCH),
                     new QQ_ActionActionPair(
                             new QQ_ActionActionList("Moving", Arrays.asList(
                                     new QQ_ActionRotateTo(0, AngleUnit.DEGREES),
                                     new QQ_ActionDriveTo(-(WAFFLE_RED_X - 8), WAFFLE_RED_Y, DistanceUnit.INCH))),
-                            new QQ_ActionLift(11.0, DistanceUnit.INCH)),
+                            new QQ_ActionLift(11.0, DistanceUnit.INCH)), // lift while we turn and drive
                     //new QQ_ActionFlipper(90),
                     new QQ_ActionPincer(false),
                     new QQ_ActionSnatcher(true), // waffle snagged
-                    new QQ_ActionDelayFor(1.25),
+                    new QQ_ActionDelayFor(1.25), // release brick and grab foundation
                     new QQ_ActionDriveTo(-(WAFFLE_RED_X + 20), WAFFLE_RED_Y - 5, DistanceUnit.INCH),
                     new QQ_ActionRotateTo(90, AngleUnit.DEGREES),
-                    new QQ_ActionDriveTo(-(WAFFLE_RED_X + 20), WAFFLE_RED_Y + 10, DistanceUnit.INCH), //to square on wall
+                    new QQ_ActionDriveTo(-(WAFFLE_RED_X + 20), WAFFLE_RED_Y + 10, DistanceUnit.INCH), //back up and square on wall
                     new QQ_ActionSnatcher(false),
-                    new QQ_ActionSetPosition(new RobotPosition(-(WAFFLE_RED_X + 13), FIELD_BOUNDARIES - (WAFFLE_WIDTH + 9), DistanceUnit.INCH, 90, AngleUnit.DEGREES)),
-                    new QQ_ActionDriveTo(-40, 24, DistanceUnit.INCH),
-                    secondStone,
-                    new QQ_ActionRotateTo(90.0, AngleUnit.DEGREES)
+                    new QQ_ActionSetPosition(new RobotPosition(-(WAFFLE_RED_X + 13), FIELD_BOUNDARIES - (WAFFLE_WIDTH + 9), DistanceUnit.INCH, 90, AngleUnit.DEGREES)), // reset where the robot thinks it is
+                    new QQ_ActionDriveTo(-40, 24, DistanceUnit.INCH), // clear alliance partner
+                    secondStone,// run second stone steps
+                    new QQ_ActionRotateTo(90.0, AngleUnit.DEGREES) // dont hit alliance partner on the way back
             ));
         }
-        steps.addAll(getParkSteps());
+        steps.addAll(getParkSteps()); // run park steps
         return steps;
     }
 
